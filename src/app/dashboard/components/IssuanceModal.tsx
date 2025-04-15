7a6199283142a18152ff14232a4dde4d9e5b3852
unknown <tashinamgay9876@gmail.com>
2025-04-15 15:59:35 +0600

    Code formatting

--- src/app/dashboard/components/IssuanceModal.tsx
+++ src/app/dashboard/components/IssuanceModal.tsx
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import Image from "next/image";
 import { TextField } from "@mui/material";
 
 const style = {
