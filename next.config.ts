5eea34292f84a6c151938a239aea26f83387436d
unknown <tashinamgay9876@gmail.com>
2025-09-12 10:46:07 +0600

    Removed source maps and logs, and moved cryptography to server

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint errors during build
   },
+  productionBrowserSourceMaps: false,
+  
+  // Additional compiler options
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+
   /* other config options */
 };
 
